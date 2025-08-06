ff71d1490cdec63918c98a68fd0f760517fcef68
Levi_Soares <levisoaresp.alho.8c.m@gmail.com>
2025-08-05 21:15:27 -0300

    Levi-Multiply/Potence-and-switch

--- calculadora/Program.cs
+++ calculadora/Program.cs
@@ -7,7 +7,32 @@ namespace CalculadoraWeb
         static void Main(string[] args)
         {
             Console.WriteLine("Calculadora Colaborativa Web");
-            // Integrante 1: Implementar menu e chamar as funções
+            System.Console.WriteLine("Escolha o calculo que deseja: ");
+
+            int escolha = int.Parse(Console.ReadLine());
+
+            switch(escolha)
+            {
+                case 1:
+                Adicao(a, b);
+                break;
+
+                case 2:
+                subtracao(a, b);
+                break;
+
+                case 3:
+                Multiplicacao(a,b);
+                break;
+
+                case 4:
+                Divisao(a, b);
+                break;
+
+                case 5:
+                Potenciacao(a, b);
+                break;
+            }
         }
 
         // Integrante 1
@@ -19,14 +44,14 @@ namespace CalculadoraWeb
         // Integrante 2
         // public static double Subtracao(double a, double b)
         // {
-        //     // Implementar aqui
+        //     // Implementar aqui 
         // }
 
-        // Integrante 3
-        // public static double Multiplicacao(double a, double b)
-        // {
-        //     // Implementar aqui
-        // }
+        
+        public static double Multiplicacao(double a, double b)
+        {
+             return a * b;
+        }
 
         // Integrante 4
         // public static double Divisao(double a, double b)
@@ -35,10 +60,25 @@ namespace CalculadoraWeb
         // }
 
 
-  // Integrante 5
-        // public static double Potenciacao(double a, double b)
-        // {
-        //     // Implementar aqui
-        // }
+  
+        public static double Potenciacao(double a, double b)
+        {
+            System.Console.WriteLine("insira a base:");
+
+            b = Console.ReadLine();
+
+            System.Console.WriteLine("insira a potência: ");
+
+            a = Console.ReadLine();
+
+            for(b; b < a; b++)
+            {
+                int calc;
+
+                calc += a * a;
+            }
+
+            System.Console.WriteLine($"O resultado é: " calc );
+        }
     }
 }
